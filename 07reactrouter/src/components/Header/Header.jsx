42971670baa7bca17343f3f0dde06793212ee351
Yash-89 <yashproraj@gmail.com>
2025-09-19 21:24:30 +0530

    added 07reactrouter

---
+++ 07reactrouter/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="shadow sticky top-0 z-50">
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5"></nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ 07reactrouter/src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router";
+import Layout from "./Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />}>
+        <Route path="company" element={<About />} />
+      </Route>
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
