0d2be2e5af9fdd9a78047c8fc8679e79edec3120
Yash-89 <yashproraj@gmail.com>
2025-09-20 13:53:02 +0530

    add Contact

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -11,15 +11,15 @@ import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import User from "./components/User/User.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
-      <Route path="about" element={<About />}>
-        <Route path="company" element={<About />} />
-      </Route>
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
       <Route path="user" element={<User />}>
         <Route path=":userId" element={<User />} />
       </Route>
